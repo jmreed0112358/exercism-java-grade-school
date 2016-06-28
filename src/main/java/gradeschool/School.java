8240164d81793a4930acb5210f11aa791e983950
Jeremy M. Reed <reeje76@gmail.com>
2016-06-27 20:13:56 -0600

    Project setup.
    Import into Eclipse.
    Convert to Maven project.
    Add Junit and assertj libraries.
    Move sources to proper packages.
    Stub out School methods.

---
+++ src/main/java/gradeschool/School.java
@@ -0,0 +1,30 @@
+package gradeschool;
+
+import java.util.List;
+import java.util.Map;
+
+import exceptions.NotImplementedException;
+
+public class School
+{
+	public School( )
+	{
+		throw new NotImplementedException();
+	}
+
+	public void add(String name, Integer grade) {
+		throw new NotImplementedException();
+	}
+	
+	public Map<Integer, List<String>> db() {
+		throw new NotImplementedException();
+	}
+	
+	public List<String> grade(Integer grade) {
+		throw new NotImplementedException();
+	}
+	
+	public Map<Integer, List<String>> sort() {
+		throw new NotImplementedException();
+	}
+}
