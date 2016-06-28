73fe9cba84074499b696fcc0b9e6fcee263d14bd
Jeremy M. Reed <reeje76@gmail.com>
2016-06-27 21:02:23 -0600

    Implement School methods.

--- src/main/java/gradeschool/School.java
+++ src/main/java/gradeschool/School.java
@@ -1,5 +1,9 @@
 package gradeschool;
 
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,24 +11,75 @@ import exceptions.NotImplementedException;
 
 public class School
 {
+	private Map<Integer, List<String>> roster;
+	
 	public School( )
 	{
-		throw new NotImplementedException();
+		this.roster = new HashMap<Integer, List<String>>();
 	}
 
+	/**
+	 * Add a new student to the roster.
+	 * @param name A non-empty, non-null String representing the student's name.
+	 * @param grade The student's grade, an Integer in the range 1-12 inclusive.
+	 */
 	public void add(String name, Integer grade) {
-		throw new NotImplementedException();
+		if ( name == null ) {
+			throw new NullPointerException();
+		} else if ( name.isEmpty( ) || !(grade >= 1 && grade <= 12) ) {
+			throw new InvalidParameterException();
+		}
+		
+		List<String> listOfStudents = this.roster.get( grade );
+		if ( listOfStudents == null ) {
+			listOfStudents = new ArrayList<String>();
+			listOfStudents.add( name );
+			this.roster.put( grade, listOfStudents );
+		} else {
+			listOfStudents.add( name );
+			this.roster.replace( grade, listOfStudents );
+		}
 	}
 	
+	/**
+	 * Returns the roster.
+	 * @return
+	 */
 	public Map<Integer, List<String>> db() {
-		throw new NotImplementedException();
+		return this.roster;
 	}
 	
+	/**
+	 * Given a grade, returns a list of students in that grade, or an empty list if there are no students in that grade.
+	 * @param grade
+	 * @return
+	 */
 	public List<String> grade(Integer grade) {
-		throw new NotImplementedException();
+		if ( !(grade >= 1 && grade <= 12) ) {
+			throw new InvalidParameterException();
+		}
+		
+		List<String> listOfStudents = this.roster.get( grade );
+		if ( listOfStudents == null ) {
+			return new ArrayList<String>();
+		} else {
+			return listOfStudents;
+		}
 	}
 	
+	/**
+	 * Sorts the roster, and returns it.
+	 * @return
+	 */
 	public Map<Integer, List<String>> sort() {
-		throw new NotImplementedException();
+		for ( int i = 1 ; i <= 12 ; i++ ) {
+			List<String> listOfStudents = this.roster.get( i );
+			if ( listOfStudents != null ) {
+				Collections.sort( listOfStudents );
+				this.roster.replace( i, listOfStudents );
+			}
+		}
+		
+		return this.roster;
 	}
 }
